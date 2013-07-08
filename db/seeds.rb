d06fe777a722433bd1365471a8906ac468bd97c3
Michael Pavling <pavling@gmail.com>
2013-07-08 16:32:36 +0100

    adding more ingredients to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-i = Ingredient.create! name: 'Egg'
+salt = Ingredient.create! name: 'Salt'
+pepper = Ingredient.create! name: 'Pepper'
+milk = Ingredient.create! name: 'Milk'
+olive_oil = Ingredient.create! name: 'Olive oil'
+egg = Ingredient.create! name: 'Egg'
+water = Ingredient.create! name: 'Water'
+butter = Ingredient.create! name: 'Butter'
 
-r = Recipe.create! name: 'Scrambled Eggs', ingredient_ids: [i.id]
-
-r = Recipe.create! name: 'Omlette', ingredient_ids: [i.id]
+r = Recipe.create! name: 'Scrambled Eggs', ingredient_ids: [egg.id, salt.id, milk.id, butter.id]
+r = Recipe.create! name: 'Omlette', ingredient_ids: [egg.id, milk.id, salt.id]
 
 
